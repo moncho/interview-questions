443e5a2a8a220b9ec68b5e3317d0cd55e7c1217e
moncho <ramon.cr@gmail.com>
2018-01-02 01:55:18 +0100

    Add BSF solution

--- udemy-11-interview-questions/two-dim-arrays/click/main.go
+++ udemy-11-interview-questions/two-dim-arrays/click/main.go
@@ -1,6 +1,9 @@
 package click
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type gameMap [][]int
 
@@ -34,6 +37,39 @@ func clickWithMemory(game gameMap, visited map[string]struct{}, i, j int) gameMa
 	return game
 }
 
+func clickBFS(game gameMap, i, j int) gameMap {
+	if game[i][j] != 0 {
+		return game
+	}
+	queue := []string{pos(i, j)}
+	return visit(game, queue)
+}
+
+func visit(game gameMap, queue []string) gameMap {
+	if len(queue) == 0 {
+		return game
+	}
+	rows := len(game)
+	columns := len(game[0])
+	i, j := parse(queue[0])
+	for row := i - 1; row <= i+1; row++ {
+		for column := j - 1; column <= j+1; column++ {
+			if row < 0 || row >= rows || column < 0 || column >= columns || game[row][column] != 0 {
+				continue
+			} else {
+				game[row][column] = -2
+				queue = append(queue, pos(row, column))
+			}
+		}
+	}
+	return visit(game, queue[1:])
+}
 func pos(x, y int) string {
 	return fmt.Sprintf("%d,%d", x, y)
+}
+
+func parse(s string) (int, int) {
+	i, _ := strconv.Atoi(s[:1])
+	j, _ := strconv.Atoi(s[2:])
+	return i, j
 }
